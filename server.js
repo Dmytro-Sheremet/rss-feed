924b0346f47be7cf7ff4877e67dc2766744dc92b
Dmytro Sheremet <dmytro.sheremet.lviv@gmail.com>
2019-03-24 19:44:52 +0200

    test

--- server.js
+++ server.js
@@ -30,8 +30,9 @@ app.use('/api/items', items);
 if (process.env.NODE_ENV === 'production') {
 	//Set static folder
 	app.use(express.static('client/build'));
+
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
 
